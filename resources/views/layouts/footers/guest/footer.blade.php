744e8bb74ad9b88a4393dd4ee52db66b0046c551
Mauro Fatu <maurofatu@hotmail.com>
2025-03-24 23:17:29 -0500

    firts commit

---
+++ resources/views/layouts/footers/guest/footer.blade.php
@@ -0,0 +1,20 @@
+  <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+  <footer class="footer py-5">
+    <div class="container">
+      {{-- <div class="row">
+      </div> --}}
+      @if (!auth()->user() || \Request::is('static-sign-up')) 
+        <div class="row">
+          <div class="col-8 mx-auto text-center mt-1">
+            <p class="mb-0 text-secondary">
+              Copyright © <script>
+                document.write(new Date().getFullYear())
+              </script> Developer by 
+              <a style="color: #252f40;" class="font-weight-bold ml-1" target="_blank">Sección Desarrollo TIC</a>
+            </p>
+          </div>
+        </div>
+      @endif
+    </div>
+  </footer>
+  <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
